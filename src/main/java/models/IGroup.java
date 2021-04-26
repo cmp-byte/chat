02806bcb141b4ada7d48f9164a1347c740f72f40
Razvan <razvancampian1999@yahoo.com>
2021-04-26 13:00:55 +0300

    First functions

--- src/main/java/models/IGroup.java
+++ src/main/java/models/IGroup.java
@@ -1,9 +1,10 @@
 package models;
 
+import java.awt.image.ImagingOpException;
 import java.io.IOException;
 
 public interface IGroup {
-    public boolean rename()throws IOException, java.sql.SQLException;
-    public boolean add() throws java.sql.SQLException;
+    public boolean rename() throws IOException,java.sql.SQLException;
+    public boolean add(User user) throws java.sql.SQLException;
     public boolean delete()throws java.sql.SQLException;
 }
